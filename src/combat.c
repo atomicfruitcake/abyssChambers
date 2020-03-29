adaa89cdba052a69d0fb27484b6b6715be1baa6a
atomicfruitcake <sam@wirewax.com>
2020-03-30 00:48:13 +0100

    Fix formatting

--- src/combat.c
+++ src/combat.c
@@ -5,51 +5,51 @@
 #include "misc.h"
 
 int checkWeaponInInventory(const char * weapon) {
-  if (inCurrentInventory(player, weapon)) {
-    return 0;
-  }
-  return 1;
+    if (inCurrentInventory(player, weapon)) {
+        return 0;
+    }
+    return 1;
 }
 
 static void attack(const char * noun, OBJECT * to,
-  const char * weapon) {
-  if (weapon == NULL) {
-    printf("You have not selected a weapon to attack with");
-    return;
-  }
-  OBJECT * wep = parseObject(weapon);
-  if (!checkWeaponInInventory(weapon)) {
-    printf("No %s found in your inventory, cannot attack", weapon);
-    return;
-  }
-
-  if (to == to -> location) {
-    switch (distanceTo(to)) {
-    case distPlayer:
-      printf("You should not be doing that to yourself.\n");
-      break;
-    default:
-      printf("You cannot attack that");
+    const char * weapon) {
+    if (weapon == NULL) {
+        printf("You have not selected a weapon to attack with");
+        return;
     }
-  } else if (to == NULL) {
-    printf("There is nothing here to attack.\n");
-  } else if (weapon == NULL) {
-    printf("I don't understand what weapon you want to attack with\n");
-  } else if (to -> health - wep -> attack > 0) {
-    to -> health = to -> health - wep -> attack;
-    player -> health = player -> health - to -> attack;
-    printf("You did %d damage to %s.\n", wep -> attack, to -> tags[0]);
-    printf("%s has %d health remaining\n", to -> tags[0], to -> health);
-    printf("\n%s fights back and does %d damage\n", to -> tags[0], to -> attack);
-    printf("You now have %d health", player -> health);
-  } else if (to -> health - wep -> attack <= 0) {
-    printf("You killed the '%s'\n", to -> tags[0]);
-    to -> health = 0;
-    to -> location = NULL;
-  }
+    OBJECT * wep = parseObject(weapon);
+    if (!checkWeaponInInventory(weapon)) {
+        printf("No %s found in your inventory, cannot attack", weapon);
+        return;
+    }
+
+    if (to == to -> location) {
+        switch (distanceTo(to)) {
+        case distPlayer:
+            printf("You should not be doing that to yourself.\n");
+            break;
+        default:
+            printf("You cannot attack that");
+        }
+    } else if (to == NULL) {
+        printf("There is nothing here to attack.\n");
+    } else if (weapon == NULL) {
+        printf("I don't understand what weapon you want to attack with\n");
+    } else if (to -> health - wep -> attack > 0) {
+        to -> health = to -> health - wep -> attack;
+        player -> health = player -> health - to -> attack;
+        printf("You did %d damage to %s.\n", wep -> attack, to -> tags[0]);
+        printf("%s has %d health remaining\n", to -> tags[0], to -> health);
+        printf("\n%s fights back and does %d damage\n", to -> tags[0], to -> attack);
+        printf("You now have %d health", player -> health);
+    } else if (to -> health - wep -> attack <= 0) {
+        printf("You killed the '%s'\n", to -> tags[0]);
+        to -> health = 0;
+        to -> location = NULL;
+    }
 }
 
 void executeAttack(const char * noun,
-  const char * weapon) {
-  attack(noun, personHere(), weapon);
+    const char * weapon) {
+    attack(noun, personHere(), weapon);
 }
--- src/torch.c
+++ src/torch.c
@@ -5,24 +5,24 @@
 #include "misc.h"
 
 int checkTorchInInventory() {
-  if (inCurrentInventory(player, "torch")) {
-    return 0;
-  }
-  return 1;
+    if (inCurrentInventory(player, "torch")) {
+        return 0;
+    }
+    return 1;
 }
 
 void executeLightTorch() {
-  if (!checkTorchInInventory()) {
-    printf("No torch found in your inventory, cannot light torch\n");
-    return;
-  }
+    if (!checkTorchInInventory()) {
+        printf("No torch found in your inventory, cannot light torch\n");
+        return;
+    }
 
-  if (player -> location == witchesLair) {
-	  torch -> lit = 0;
-	  torch -> description = "A flaming torch allowing you to travel to darker places";
-	  torch -> details = "The torch is alight, allowing you to venture to darker places";
-	  printf("You have lit the torch. You can now venture into darker places");
-	  return;
-  }
-  printf("Cannot light the torch here, there is no flame around");
+    if (player -> location == witchesLair) {
+        torch -> lit = 0;
+        torch -> description = "A flaming torch allowing you to travel to darker places";
+        torch -> details = "The torch is alight, allowing you to venture to darker places";
+        printf("You have lit the torch. You can now venture into darker places");
+        return;
+    }
+    printf("Cannot light the torch here, there is no flame around");
 }
